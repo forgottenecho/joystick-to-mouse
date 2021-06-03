172ce512077237b769f8fa71b32ea4f2047f60bd
forgottenecho <39808231+forgottenecho@users.noreply.github.com>
2021-06-03 07:50:39 -0500

    mouse moves as joystick moves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import joystick, event, JOYAXISMOTION
 import pygame
+import mouse
 
 pygame.init()
 
@@ -12,12 +13,38 @@ print(stick.get_numballs())
 print(stick.get_numhats())
 print(stick.get_numbuttons())
 # event.init(, 11)
+x=0
+y=0
+slow_factor=.1
+threshold=.1*slow_factor
+mouse.move(x,y)
 while 1:
     e=event.get()
     # if e.type == JOYAXISMOTION:
         # print('hello')
-    axis=0
-    value=stick.get_axis(axis)
+    dx=stick.get_axis(0)*slow_factor
+    dy=stick.get_axis(1)*slow_factor
+    if abs(dx) < threshold:
+        dx=0
+    if abs(dy) < threshold:
+        dy=0
+    # print('{}, {}'.format(x,y))
+    x+=dx
+    y+=dy
+    if x < 0:
+        x-=dx
+    if y < 0:
+        y-=dy
+    mouse.move(x,y)
+    
+    if stick.get_button(0) == 1 and last_state[0] == 0:
+        mouse.press(button='left')
+    if stick.get_button(0) == 0 and last_state[0] == 1:
+        mouse.release(button='left')
+    # for i in range(stick.get_numbuttons()):
+        # if stick.get_button(i) == 1:
+            # print(i)
+    # value=stick.get_axis(axis)
     # value=stick.get_hat(axis)
-    if value:
-        print(value)
+    # if value:
+        # print(value)
