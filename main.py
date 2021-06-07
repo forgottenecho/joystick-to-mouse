2875e1a4e0ec9f4d86fd504865becd96b86ba346
averagephotographer <averagephotographer@users.noreply.github.com>
2021-06-06 20:32:12 -0500

    more commenting

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from pygame import joystick, event, JOYAXISMOTION
+# from pygame import joystick, event, JOYAXISMOTION
 import pygame
 # import mouse
 
 
 pygame.init()
 
-joystick.init()
-print(joystick.get_count())
-stick=joystick.Joystick(0)
+pygame.joystick.init()
+print(pygame.joystick.get_count())
+stick=pygame.joystick.Joystick(0)
 stick.init()
 print(stick.get_numaxes())
 print(stick.get_numballs())
@@ -21,7 +21,7 @@ slow_factor=.1
 threshold=.1*slow_factor
 #mouse.move(x,y)
 while 1:
-    e=event.get()
+    e=pygame.event.get()
     # if e.type == JOYAXISMOTION:
         # print('hello')
     dx=stick.get_axis(0)*slow_factor
@@ -42,10 +42,10 @@ while 1:
     current_state=[stick.get_button(0),0]
     if current_state[0] and last_state[0] == 0:
         #mouse.press(button='left')
-        # print('press')
+        print('press')
     if current_state[0] == 0 and last_state[0] == 1:
         #mouse.release(button='left')
-        # print('release')
+        print('release')
     last_state=current_state.copy()
     # for i in range(stick.get_numbuttons()):
         # if stick.get_button(i) == 1:
